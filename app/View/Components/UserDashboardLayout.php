2e335e1f9c97b5f9e785065fc3322beb0c0de087
mahethekiller <abc@gmail.com>
2025-05-22 18:04:56 +0530

    added user dashboard2222

--- app/View/Components/UserDashboardLayout.php
+++ app/View/Components/UserDashboardLayout.php
@@ -7,14 +7,6 @@ use Illuminate\View\View;
 class UserdashboardLayout extends Component
 {
     /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        // Initialization logic if necessary
-    }
-
-    /**
      * Get the view / contents that represent the component.
      */
     public function render(): View
