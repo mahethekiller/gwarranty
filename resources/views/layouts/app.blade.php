5fac3978faf74f978efa8b6b49c443a5a15c1b04
mahethekiller <abc@gmail.com>
2025-05-22 16:46:48 +0530

    added user dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,16 +29,6 @@
             </header>
         @endisset
 
-
-        <!-- Page Heading -->
-        @isset($mk)
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $mk }}
-                </div>
-            </header>
-        @endisset
-
         <!-- Page Content -->
         <main>
             {{ $slot }}
