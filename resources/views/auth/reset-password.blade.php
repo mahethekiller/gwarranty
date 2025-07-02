9e941dec2ac7a65fabaea05b3538b8fa31e4b9b7
mahethekiller <abc@gmail.com>
2025-07-02 12:00:37 +0530

    admin Login pages

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,44 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+       <x-common-layout>
+           <div class="row align-items-center form-login-bg">
+               <div class="col-md-12 col-lg-12">
+                   <form method="POST" class="signin-left" action="{{ route('password.store') }}">
+                       @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                       <!-- Password Reset Token -->
+                       <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                       <!-- Email Address -->
+                       <div class="form-group">
+                           <x-input-label for="email" :value="__('Email')" />
+                           <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                               :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                           <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                       </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                       <!-- Password -->
+                       <div class="form-group">
+                           <x-input-label for="password" :value="__('Password')" />
+                           <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"
+                               required autocomplete="new-password" />
+                           <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                       </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                       <!-- Confirm Password -->
+                       <div class="form-group">
+                           <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+                           <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                               name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                           <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                       </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                       <div class="flex items-center justify-end mt-4">
+                           <x-primary-button class="custom-btn3">
+                               {{ __('Reset Password') }}
+                           </x-primary-button>
+                       </div>
+                   </form>
+               </div>
+           </div>
+       </x-common-layout>
